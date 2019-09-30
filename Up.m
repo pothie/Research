81a3ee223927702357274d42c368701d24fa0149
chen8456vis2019A <41704405+pothie@users.noreply.github.com>
2019-09-30 00:13:09 -0400

    Update Up.m

--- Up.m
+++ Up.m
@@ -1,10 +1,16 @@
-function y = Up(x)
+% x: x-values
+% pt: 4x2 matrix with 4 turning points
+function y = Up(x,pt)
     y = zeros(size(x));
-    x0 = 0.1;
-    x1 = 0.9;
-    x2 = 1;
-    y(x<=x0) = 400*x(x<=x0);
-    y(x0<x & x<=x1) = 40;
-    y(x1<x & x<=x2) = -400*x(x1<x&x<=x2)+400;
-    y(x>x2) = 0;
+    x1 = pt(1,1);
+    x2 = pt(2,1);
+    x3 = pt(3,1);
+    x4 = pt(4,1);
+    y1 = pt(1,2);
+    y2 = pt(2,2);
+    y(x<=x1) = y1;
+    y(x1<x & x<=x2) = (y2-y1)/(x2-x1)*(x(x1<x & x<=x2)-x1)+y1;
+    y(x2<x & x<=x3) = y2;
+    y(x3<x & x<=x4) = (y1-y2)/(x4-x3)*(x(x3<x & x<=x4)-x3)+y2;
+    y(x>x4) = y1;
 end

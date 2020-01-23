f746b32c706ddc459f11439c6fea017a8c6d010f
Yidi <41704405+pothie@users.noreply.github.com>
2020-01-22 23:21:32 -0500

    Add files via upload

--- DG1DSysTest.m
+++ DG1DSysTest.m
@@ -10,13 +10,12 @@ VX=(xmax-xmin)*(0:N)/N+xmin;r=LegendreGL(m);
 x=ones(m+1,1)*VX(1:N)+0.5*(r+1)*(VX(2:N+1)-VX(1:N));
 h=(xmax-xmin)/N;
 %Define initial conditions
-%u=sin(2*pi*x)+0.8;%periodicBCneeded
-pt = [0.25 10;0.5 50;1 50;1.25 10];
-u(1:2,:) = 0.8*Up(x,pt);
-u(3:4,:) = 0.1*Up(x,pt);
-%u=(1-sign(x-0.2))/2+1;%ConstantBCneeded
-%SolveProblem
 pce = [1 2];
+pt = [0.25 10;0.5 50;1 50;1.25 10];
+dis = [0.8 0.2]./pce;
+u(1:2,:) = dis(1)*Up(x,pt);
+u(3:4,:) = dis(2)*Up(x,pt);
+%SolveProblem
 vmax = [90;120]; %120km/h
 k0 = 50; % 50 veh/km
 % fundamental relation

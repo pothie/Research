d876d227bdac5f940e432cc7e2646a158e0d63c8
Yidi <41704405+pothie@users.noreply.github.com>
2020-02-13 19:43:51 -0500

    Add files via upload

--- CU3.m
+++ CU3.m
@@ -5,7 +5,7 @@
 % dv: derivative of speed
 % q: flow
 % pce: pce values of different classes
-function [U,U1,U2,tgrid] = CU3(x,T,ux0,v,dv,q,pce)
+function [U,U1,U2,tgrid] = CU3(x,T,ux0,v,dv,q,xT)
     %pt = [0.25 10;0.5 50;1 50;1.25 10];
     %CFL = 0.7;
     dx = x(2)-x(1);
@@ -15,7 +15,7 @@ function [U,U1,U2,tgrid] = CU3(x,T,ux0,v,dv,q,pce)
     U2 = U;
     U1(:,1) = ux0(:,1);
     U2(:,1) = ux0(:,2);
-    U(:,1) = pce*[U1(:,1) U2(:,1)]';
+    U(:,1) = xT(U1(:,1),U2(:,1));
     
     tstep = 1;
     tgrid(tstep) = 0;
@@ -25,7 +25,7 @@ function [U,U1,U2,tgrid] = CU3(x,T,ux0,v,dv,q,pce)
         Up1 = U1(:,tstep);
         Up2 = U2(:,tstep);
         
-        [uav1,uav2,dt] = CUscheme2(Up1,Up2,dx,q,dv,v);
+        [uav1,uav2,dt] = CUscheme2(Up1,Up2,dx,q,dv,v,xT);
         
         tpass = tgrid(tstep);
         if tpass+dt>T
@@ -34,36 +34,56 @@ function [U,U1,U2,tgrid] = CU3(x,T,ux0,v,dv,q,pce)
         % RK2 k1
         k1_1 = dt*(uav1);
         k1_2 = dt*(uav2);
-       
+        
         % calculating k2 = un+k1/2
         Up1(2:end-1) = Up1(2:end-1)+k1_1;
         Up2(2:end-1) = Up2(2:end-1)+k1_2;
+        Up1(end) = Up1(end)-...
+            (dt/dx)*(q(Up1(end),xT(Up1(end),Up2(end)),1)-q(Up1(end-1),xT(Up1(end-1),Up2(end-1)),1));
+        Up2(end) = Up2(end)-...
+            (dt/dx)*(q(Up2(end),xT(Up1(end),Up2(end)),2)-q(Up2(end-1),xT(Up1(end-1),Up2(end-1)),2));
+        Up1(1) = Up1(2);
+        Up2(1) = Up2(2);
         
-        [uav1,uav2,~] = CUscheme2(Up1,Up2,dx,q,dv,v);
+        [uav1,uav2,~] = CUscheme2(Up1,Up2,dx,q,dv,v,xT);
         k2_1 = dt*(uav1);
         k2_2 = dt*(uav2);
         Up1= U1(:,tstep)*0.75+Up1*0.25;
         Up2= U2(:,tstep)*0.75+Up2*0.25;
         Up1(2:end-1)= Up1(2:end-1)+k2_1/4;
         Up2(2:end-1)= Up2(2:end-1)+k2_2/4;
+        Up1(end) = Up1(end)-...
+            (dt/dx)*(q(Up1(end),xT(Up1(end),Up2(end)),1)-q(Up1(end-1),xT(Up1(end-1),Up2(end-1)),1));
+        Up2(end) = Up2(end)-...
+            (dt/dx)*(q(Up2(end),xT(Up1(end),Up2(end)),2)-q(Up2(end-1),xT(Up1(end-1),Up2(end-1)),2));
+        Up1(1) = Up1(2);
+        Up2(1) = Up2(2);
         
         %put BC here, missing?
-        [uav1,uav2,~] = CUscheme2(Up1,Up2,dx,q,dv,v);
+        [uav1,uav2,~] = CUscheme2(Up1,Up2,dx,q,dv,v,xT);
         k3_1 = dt*(uav1);
         k3_2 = dt*(uav2);
         Up1= U1(:,tstep)/3+Up1*2/3;
         Up2= U2(:,tstep)/3+Up2*2/3;
-        U1(2:end-1,tstep+1)= Up1(2:end-1)+k3_1*2/3;
-        U2(2:end-1,tstep+1)= Up2(2:end-1)+k3_2*2/3;
+        Up1(2:end-1)= Up1(2:end-1)+k3_1*2/3;
+        Up2(2:end-1)= Up2(2:end-1)+k3_2*2/3;
 
-        %Boundary points U(1,:) = Up(tpass,pt)
-        U1(end,tstep+1) = U1(end-1,tstep+1);
-        U2(end,tstep+1) = U2(end-1,tstep+1);
-
-        U1(1,tstep+1) = U1(2,tstep+1);%0.8*Up(tpass,pt);%U1(2,tstep+1);
-        U2(1,tstep+1) = U2(2,tstep+1);%0.1*Up(tpass,pt);%U2(2,tstep+1);
-%         
-        U(:,tstep+1) = pce*[U1(:,tstep+1) U2(:,tstep+1)]'; 
+        %Boundary points
+        Up1(end) = Up1(end)-...
+            (dt/dx)*(q(Up1(end),xT(Up1(end),Up2(end)),1)-q(Up1(end-1),xT(Up1(end-1),Up2(end-1)),1));
+        Up2(end) = Up2(end)-...
+            (dt/dx)*(q(Up2(end),xT(Up1(end),Up2(end)),2)-q(Up2(end-1),xT(Up1(end-1),Up2(end-1)),2));
+        
+        Up1(1) = U1(1,tstep);%0.8*Up(tpass,pt);%U1(2,tstep+1);
+        Up2(1) = U2(1,tstep);%0.1*Up(tpass,pt);%U2(2,tstep+1);
+       
+        U1(:,tstep+1) = Up1;
+        U2(:,tstep+1) = Up2;
+        
+        %if tstep > 1020
+        %    disp("stop")
+        %end
+        U(:,tstep+1) = xT(U1(:,tstep+1),U2(:,tstep+1)); 
         
         tgrid(tstep+1) = tpass+dt;
         tstep = tstep+1;

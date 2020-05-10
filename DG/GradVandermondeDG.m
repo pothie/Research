3a126dc3b6a94499b3d0f9bb6f4af7882a04ad45
Yidi <41704405+pothie@users.noreply.github.com>
2020-05-10 04:48:29 -0400

    Update and rename GradVandermondeDG.m to DG/GradVandermondeDG.m

--- DG/GradVandermondeDG.m
+++ DG/GradVandermondeDG.m
@@ -1,7 +1,7 @@
 function[Vr]=GradVandermondeDG(m,r)
 %function[Vr]=GradVandermondeDG(m,r)
-%Purpose:InitializethegradientoftheVandermondematrix
-%ofordermat(r)
+%Purpose:Initialize the gradient of the Vandermonde matrix
+%of order m at (r)
 Vr=zeros(length(r),(m+1));
 for i=0:m
 [Vr(:,i+1)]=GradLegendreP(r(:),i);

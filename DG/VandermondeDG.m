bfc71758fd9089fa7d4fd6699bf509513befab60
Yidi <41704405+pothie@users.noreply.github.com>
2020-05-10 21:23:41 -0400

    Update and rename VandermondeDG.m to DG/VandermondeDG.m

--- DG/VandermondeDG.m
+++ DG/VandermondeDG.m
@@ -1,6 +1,7 @@
 function[V]=VandermondeDG(m,r)
 %function[V]=VandermondeDG(m,r)
-%Purpose:Initializethe1DVandermondeMatrix,V_{ij}=phi_j(r_i);
+%Purpose:Initialize the 1D Vandermonde Matrix,
+% V_{ij} = phi_j(r_i);
 V=zeros(length(r),m+1);
 for j=1:m+1
 V(:,j)=LegendreP(r(:),j-1);
